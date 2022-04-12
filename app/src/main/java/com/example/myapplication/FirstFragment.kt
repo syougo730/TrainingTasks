2384b47963a1d0fd7bf763a44a69f7774a2157e7
meta_shogo.h <shogo.honma@metamoji.com>
2022-04-12 16:45:30 +0900

    fragmentで保存

--- app/src/main/java/com/example/myapplication/FirstFragment.kt
+++ app/src/main/java/com/example/myapplication/FirstFragment.kt
@@ -4,80 +4,38 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
-import com.example.myapplication.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FirstFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null //バッキングプロパティ 参照するときに参照するものはnullではないことを強制するためのもの
-
-    private var buttonFlg = 0
-    private val buttonTexts = arrayOf("ほげ","ふが")
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
-    private val binding get() = _binding!!
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        println(" -- onCreate - fragment -- ")
-
-        if(savedInstanceState != null){
-            buttonFlg = savedInstanceState.getInt("buttonFlg")
-        }
-        println(" buttonFlg -> $buttonFlg ")
-
-    }
+    var buttonFlg = 1
+    val buttonTexts = arrayOf("ほげ","ふが")
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
-        println("-- onCreateView --")
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        val view = inflater.inflate(
+            R.layout.fragment_first,
+            container, false
+        )
 
-        println("binding.root -> ${binding.root}")
-
-        val button = binding.buttonFirst
-        button.text = buttonTexts[buttonFlg]
-
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        println("-- onViewCreated --")
-        println("buttonFlg  -> $buttonFlg")
-
-        binding.buttonFirst.setOnClickListener {
+        var button = view.findViewById<Button>(R.id.button_first)
+        button.setOnClickListener {
             when(buttonFlg){
                 0 -> buttonFlg = 1
                 1 -> buttonFlg = 0
             }
-            val button = binding.buttonFirst
             button.text = buttonTexts[buttonFlg]
-            println(" buttonFlg -> $buttonFlg ")
-            //findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
+
+        return view
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        println(" -- onDestroyView - fragment -- ")
-        _binding = null
-    }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        println("-- onSaveInstanceState --")
-        outState.putInt("buttonFlg", buttonFlg)
-        println(" buttonFlag(onSIS) -> $buttonFlg")
-    }
 }
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,47 +2,13 @@ package com.example.myapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentTransaction
-import androidx.navigation.ui.AppBarConfiguration
-import com.example.myapplication.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        println(" -- onCreate -- ")
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        //setContentView(binding.root)
-
         setContentView(R.layout.activity_main)
-
-        if(savedInstanceState == null){
-            //Fragmentを作成します
-            val fragment = FirstFragment()
-            // Fragmentの追加や削除といった変更を行う際は、Transactionを利用します
-            val transaction: FragmentTransaction = supportFragmentManager.beginTransaction()
-            transaction.add(R.id.container, fragment)
-            // 最後にcommitを使用することで変更を反映します
-            transaction.commit()
-        }
-
     }
-    override fun onPause() {
-        super.onPause()
-        println("-- onPause --")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        println("-- onResume --")
-    }
-
-
 
 }
